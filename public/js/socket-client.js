f288ca22b8b1be85c4339f0a3d715b8dcb8550a5
Gerardot45 <63618451+Gerardot45@users.noreply.github.com>
2022-04-08 10:18:45 -0500

    Aprendiendo sobre sockets

---
+++ public/js/socket-client.js
@@ -0,0 +1,17 @@
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+
+const socket = io();
+
+socket.on("connect", () => {
+  console.log("conectado");
+
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  console.log("desconectado");
+  lblOffline.style.display = "";
+  lblOnline.style.display = "none";
+});
